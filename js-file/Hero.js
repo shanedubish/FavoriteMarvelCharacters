de5cd3360fadfebc459b61257b7b51ae0ac127b2
MyMac <mymac@MyMacs-MBP.frontierlocal.net>
2020-05-15 18:05:03 -0400

    more work on js

--- js-file/Hero.js
+++ js-file/Hero.js
@@ -1,19 +1,27 @@
 console.log('hello world');
 $(() => {
+
+$('#info').on('click',  () => {
+  event.preventDefault()
+let nameInput = $('input[type="text"]').val()
+
 $.ajax({
-  url:`https://cors-anywhere.herokuapp.com/superheroapi.com/api/10215488179708651/search/batman`,
+  url:`https://cors-anywhere.herokuapp.com/superheroapi.com/api/10215488179708651/search/${nameInput}`,
 
 })
 .then(
-  (data) => {
-    console.log(data);
+  (info) => {
+    console.log(info[0]);
+const $stats = $('.info')
+const $statDiv = $('<div>').appendTo($stats)
+const $appear = $('<p>').text(info[0]).appendTo($statDiv)
   }
 )
 .catch(err=> {
   console.log(err);
 })
 
-
+})
 
 
 })
