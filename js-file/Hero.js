8d676035afb1b9790c4cd898b23fa4d11d17b931
MyMac <mymac@MyMacs-MBP.frontierlocal.net>
2020-05-17 23:33:05 -0400

    added more css and got picture veiwer working

--- js-file/Hero.js
+++ js-file/Hero.js
@@ -1,9 +1,11 @@
 console.log("hello world");
 $(() => {
+  let currentIndex = 0;
+
   $(".leftbutton").hide();
   $(".rightbutton").hide();
-  $(".noname").hide();
-  $("#info").on("click", () => {
+
+  const start = () => {
     event.preventDefault();
     $(".picture").empty();
     $(".info").empty();
@@ -15,41 +17,79 @@ $(() => {
       type: "Get",
     })
       .then((info) => {
-        console.log(info.results[0]);
-        $(".leftbutton").show();
-        $(".rightbutton").show();
-        const $stats = $(".info");
-        const $statlist = $("<div>").appendTo($stats).addClass("stat");
-        const $name = $("<p>").text(info.results[0].name).appendTo($statlist);
+        const slideNum = info.results.length - 1;
+        $(".rightbutton").on("click", () => {
+          console.log(currentIndex);
+          $(".picture").empty();
+          $(".info").empty();
+          if (currentIndex < slideNum) {
+            currentIndex++;
+          } else {
+            currentIndex = 0;
+          }
+          currentIndex;
+          getStuff();
+        });
 
-        const $strength = $("<p>")
-          .text(`Strength: ${info.results[0].powerstats.strength}`)
-          .appendTo($statlist);
+        $(".leftbutton").on("click", () => {
+          console.log(currentIndex);
+          $(".picture").empty();
+          $(".info").empty();
+          if (currentIndex > 0) {
+            currentIndex--;
+          } else {
+            currentIndex = slideNum;
+          }
+          currentIndex;
+          getStuff();
+        });
 
-        const $speed = $("<p>")
-          .text(`Speed: ${info.results[0].powerstats.speed}`)
-          .appendTo($statlist);
+        console.log(info.results[currentIndex]);
+        const getStuff = () => {
+          $(".leftbutton").show();
+          $(".rightbutton").show();
+          const $stats = $(".info");
+          const $statlist = $("<div>").appendTo($stats).addClass("stat");
+          const $name = $("<p>")
+            .text(info.results[currentIndex].name)
+            .appendTo($statlist);
 
-        const $power = $("<p>")
-          .text(`Power: ${info.results[0].powerstats.power}`)
-          .appendTo($statlist);
+          const $strength = $("<p>")
+            .text(`Strength: ${info.results[currentIndex].powerstats.strength}`)
+            .appendTo($statlist);
 
-        const $intelligence = $("<p>")
-          .text(`Intelligence: ${info.results[0].powerstats.intelligence}`)
-          .appendTo($statlist);
+          const $speed = $("<p>")
+            .text(`Speed: ${info.results[currentIndex].powerstats.speed}`)
+            .appendTo($statlist);
 
-        const $imagePlace = $(".picture");
-        const $image = $("<img>")
-          .attr("src", info.results[0].image.url)
-          .addClass("coolpic")
-          .appendTo($imagePlace);
+          const $power = $("<p>")
+            .text(`Power: ${info.results[currentIndex].powerstats.power}`)
+            .appendTo($statlist);
+
+          const $intelligence = $("<p>")
+            .text(
+              `Intelligence: ${info.results[currentIndex].powerstats.intelligence}`
+            )
+            .appendTo($statlist);
+
+          const $imagePlace = $(".picture");
+          const $image = $("<img>")
+            .attr("src", info.results[currentIndex].image.url)
+            .addClass("coolpic")
+            .appendTo($imagePlace);
+        };
+        getStuff();
       })
       .catch((err) => {
         console.log(err);
-        $(".noname").toggle();
+        const errorimg = $("<img>")
+          .attr("src", "img/skull.png")
+          .appendTo(".noname");
         const errorMessage = $("<p>")
           .text("Name not found")
+          .addClass("not-found")
           .appendTo(".picture");
       });
-  });
+  };
+  $("#info").on("click", start);
 });
